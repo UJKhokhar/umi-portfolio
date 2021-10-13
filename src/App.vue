f7fd071c0a31ffd9d52a2b8b8c67e451439a19ae
Umar Khokhar <ujkhokhar@gmail.com>
2021-10-13 15:12:53 -0400

    Add sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UmiHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UmiHeader from './components/UmiHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UmiHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #000000;
+  color: #ffffff;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/UmiHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="umi-header">
+    <nav>
+      <h1>Umi</h1>
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <path d="M90 7a3 3 0 0 1 3 3v80a3 3 0 0 1-3 3H10a3 3 0 0 1-3-3V10a3 3 0 0 1 3-3h80m0-7H10A10 10 0 0 0 0 10v80a10 10 0 0 0 10 10h80a10 10 0 0 0 10-10V10A10 10 0 0 0 90 0Z"/>
+        <rect x="20" y="20" width="60" height="10" rx="5"/>
+        <rect x="20" y="45" width="60" height="10" rx="5"/>
+        <rect x="20" y="70" width="60" height="10" rx="5"/>
+      </svg>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UmiHeader',
+}
+</script>
+
+<style lang="scss">
+  nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 40px;
+  }
+
+  svg {
+    fill: #ffffff;
+    width: 25px;
+  }
+</style>
